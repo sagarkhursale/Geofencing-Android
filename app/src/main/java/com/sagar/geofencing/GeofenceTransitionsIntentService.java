02532a44659b128ec2aff669f23742435e9cfe4c
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-12 02:45:16 +0530

    update onHandleIntent()

--- app/src/main/java/com/sagar/geofencing/GeofenceTransitionsIntentService.java
+++ app/src/main/java/com/sagar/geofencing/GeofenceTransitionsIntentService.java
@@ -1,9 +1,17 @@
 package com.sagar.geofencing;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
 
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GeofenceTransitionsIntentService extends IntentService {
@@ -16,18 +24,66 @@ public class GeofenceTransitionsIntentService extends IntentService {
     }
 
 
-
     @Override
     public void onCreate() {
         super.onCreate();
     }
 
 
-
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent != null && geofencingEvent.hasError()) {
+            String errorMessages = GeofenceErrorMessages.getErrorString(this, geofencingEvent.getErrorCode());
+            Log.e(TAG, errorMessages);
+        }
+
+        // get transition type
+        assert geofencingEvent != null;
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // get the geofences that were triggered..
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            // get transition details as a string
+            String geofenceTransitionDetail = getGeofenceTransitionDetails(
+                    this,
+                    geofenceTransition,
+                    triggeringGeofences
+            );
+        }
+    }
 
 
+    private String getGeofenceTransitionDetails(Context context, int geofenceTransition,
+                                                List<Geofence> triggeringGeofences) {
+
+        String geofenceTransitionString = getTransitionString(geofenceTransition);
+
+        // get ID's of each geofene that was triggered
+        ArrayList<String> triggeringGeofencesIdsList = new ArrayList<String>();
+        for (Geofence geofence : triggeringGeofences) {
+            triggeringGeofencesIdsList.add(geofence.getRequestId());
+        }
+
+        String triggeringGeofencesIdsString = TextUtils.join(", ", triggeringGeofencesIdsList);
+
+        return geofenceTransitionString + ": " + triggeringGeofencesIdsString;
+    }
+
+
+    private String getTransitionString(int transitionType) {
+        switch (transitionType) {
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                return getString(R.string.geofence_transition_entered);
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                return getString(R.string.geofence_transition_exited);
+            default:
+                return getString(R.string.unknown_geofence_transition);
+        }
     }
 
 
