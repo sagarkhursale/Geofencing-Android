cae2246fd44d93a34c57511cd5348a760dd209db
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-12 03:36:28 +0530

    getLocation using location-service in MainActivity

--- app/src/main/java/com/sagar/geofencing/MainActivity.java
+++ app/src/main/java/com/sagar/geofencing/MainActivity.java
@@ -1,15 +1,35 @@
 package com.sagar.geofencing;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,LocationListener {
+
     private static final String TAG = MainActivity.class.getSimpleName();
-
     private Button mAddGeofenceButton;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mLastLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +38,82 @@ public class MainActivity extends AppCompatActivity {
 
         mAddGeofenceButton = findViewById(R.id.add_geofences_button);
 
+        buildGoogleApiClient();
+
         //
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected())
+            mGoogleApiClient.disconnect();
     }
 
 
     public void addGeofencesButtonHandler(View view) {
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "GoogleApiClient onConnected");
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000); // location update time
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                Log.i(TAG, "Last Location : " + mLastLocation.toString());
+                updateUI(mLastLocation);
+            }
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest,  this);
+        }
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "GoogleApiClient connection has been suspended.");
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "GoogleApiClient connection has failed.");
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, location.toString());
+        updateUI(location);
+    }
+
+
+
+    private void updateUI(Location location) {
+        Log.i(TAG, "updateUI : "+location.toString());
+        //text_Latitude.setText(String.valueOf(location.getLatitude()));
+        //text_Longitude.setText(String.valueOf(location.getLongitude()));
     }
 
 
