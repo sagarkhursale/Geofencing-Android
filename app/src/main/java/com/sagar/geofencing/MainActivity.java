c28d1b4e134bc929fae69c8d432c0a765a76e5bc
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-12 01:44:53 +0530

    activity_main layout

--- app/src/main/java/com/sagar/geofencing/MainActivity.java
+++ app/src/main/java/com/sagar/geofencing/MainActivity.java
@@ -2,12 +2,29 @@ package com.sagar.geofencing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mAddGeofenceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAddGeofenceButton = findViewById(R.id.add_geofences_button);
+
+        //
     }
+
+
+    public void addGeofencesButtonHandler(View view) {
+    }
+
+
+    // END
 }
