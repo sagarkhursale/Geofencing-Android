3312020dad5a0f1a5af8b1cf44629ca8704f180b
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-12 01:54:54 +0530

    geofence intent service

---
+++ app/src/main/java/com/sagar/geofencing/GeofenceTransitionsIntentService.java
@@ -0,0 +1,35 @@
+package com.sagar.geofencing;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+
+
+public class GeofenceTransitionsIntentService extends IntentService {
+
+    private static final String TAG = "gfservice";
+
+
+    public GeofenceTransitionsIntentService() {
+        super(TAG);
+    }
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        
+
+    }
+
+
+    // end
+}
